99d7991c6142d1d847b1719f53db89044226fe68
Jakub Holy <jakubholy@jakubholy.net>
2021-03-04 10:24:04 +0100

    Better docs, local build support

--- build-locally.sh
+++ build-locally.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
+echo "Building the site based on antora-playbook-local.yml..."
 #docker run --rm -it -v "$PWD:/antora:Z" antora/antora ash # troubleshooting shell
-echo "BEWARE: Remember to have changed antora-playbook.yml to use docker-local paths instead of GH urls to see any local changes."
-docker run --rm -it -v "$PWD:/antora:Z" antora/antora antora-playbook.yml
-#docker run --rm -it -v "$(pwd | xargs dirname):/fulcro-community:Z" -v "$PWD:/antora:Z" antora/antora --stacktrace antora-playbook.yml
+#docker run --rm -it -v "$PWD:/antora:Z" antora/antora antora-playbook.yml
+docker run --rm -it -v "$(pwd | xargs dirname):/fulcro-community:Z" -v "$PWD:/antora:Z" antora/antora antora-playbook-local.yml
 echo  "Done. See ./build/site/"
